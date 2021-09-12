5975390aa093f11cc2bd43845df68621cd882b91
molinitomario <unknownliteracy729videogames0@gmail.com>
2021-09-12 09:26:58 -0400

    Updated steps to get quizGame running

--- QuizGame/README.md
+++ QuizGame/README.md
@@ -2,3 +2,19 @@
 Not done, yet.
 Currently my project is visually not that pleasing, which will be fixed once I finished implementing all the backend functionality.
 Backend first, then frontend? No?
+
+## To get running:
+Install express and mongoose
+```
+npm install express
+npm install mongoose
+```
+
+## Create quizGame database
+```
+mongo
+use quizGame
+exit
+```
+
+After following steps above it should run.
--- QuizGame/index.js
+++ QuizGame/index.js
@@ -1,24 +1,13 @@
 const express = require("express");
-<<<<<<< HEAD
-=======
 const ApplicationError = require("./ApplicationError");
 const TestRouter = require("./routers/Test");
 const AuthorizationRouter = require("./routers/Authorization");
 const ScoreRouter = require("./routers/Scores");
 const PreferencesRouter = require("./routers/Preferences");
->>>>>>> 9e2c8126be29cf45c6b48db23e5726a78bbf4047
 const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
-<<<<<<< HEAD
-app.set("views", path.join(__dirname, "views"));
-// app.use(helmet({contentSecurityPolicy: false}));
-
-app.get("/", (req, res)=>{
-  res.render("index");
-});
-=======
 app.set("views", path.join(__dirname, "/views"));
 
 ///// Middleware
@@ -29,7 +18,7 @@ app.use(PreferencesRouter);
 app.use(ScoreRouter);
 
 app.get("/", (req, res)=>{
-    console.log(req.session.userId);
+    console.log('something: ', req.session.userId);
     if (req.session.userId)
         res.redirect("/home");
     else
@@ -48,5 +37,4 @@ app.use((err, req, res, next)=>{
     res.render("error", {message, status, where});
 });
 
->>>>>>> 9e2c8126be29cf45c6b48db23e5726a78bbf4047
 app.listen(3000, ()=>{console.log("Listening on port 3000")});
